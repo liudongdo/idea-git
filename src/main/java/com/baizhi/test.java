8d3d5c95660656ad9d6de1c074578d69038c8c3f
shuai <731324870@qq.com>
2018-10-19 20:17:09 +0800

    this is test one

---
+++ src/main/java/com/baizhi/test.java
@@ -0,0 +1,10 @@
+package com.baizhi;
+
+/**
+ * Created by Administrator on 2018/10/19.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+    }
+}
