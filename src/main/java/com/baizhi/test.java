a770f19862df21a8cb6a00db50e626b443a05a1b
shuai <731324870@qq.com>
2018-10-19 20:35:58 +0800

    this is test

--- src/main/java/com/baizhi/test.java
+++ src/main/java/com/baizhi/test.java
@@ -7,6 +7,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hahaha");
         System.out.println("ddff");
+        System.out.println("这是分支的地盘");
 
     }
 }
